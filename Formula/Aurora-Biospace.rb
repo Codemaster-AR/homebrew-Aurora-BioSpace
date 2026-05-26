492458e2d099d8a170740f9d24b47052c7847cad
Anay Rustogi <codemaster.ar@gmail.com>
2026-05-26 18:09:58 +0400

    Rename class from AuroraBioscience to Aurora-Biospace

--- Formula/Aurora-Biospace.rb
+++ Formula/Aurora-Biospace.rb
@@ -1,4 +1,4 @@
-class AuroraBioscience < Formula
+class Aurora-Biospace < Formula
   desc "Launcher for the Aurora Bioscience Dashboard by Codemaster-AR."
   homepage "https://github.com/Codemaster-AR/aurora-biospace"
   url "https://github.com/Codemaster-AR/aurora/archive/refs/tags/v6.0.0.tar.gz"
